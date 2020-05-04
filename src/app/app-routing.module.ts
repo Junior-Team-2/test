a07bf97226e67945a454b6f2931164cf2f1700aa
ThihanWinhtet <thihanwinhtet1999@gmail.com>
2020-05-04 15:12:41 +0630

    abc

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { url } from 'inspector';
+
+
+const routes: Routes = [
+  {
+    path:'',
+    redirectTo :'/products',
+    pathMatch:'full'
+  },
+  {
+    path:'products',
+    loadChildren:'./product/product.module#ProductModule'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
